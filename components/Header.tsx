79a91626b91c263d6f3d7f765ec1f779745ad5b5
JASON DOOKARUN <jason.dookarun@live.co.uk>
2022-09-01 23:01:27 +0100

    modification of top header bar + thirdweb deployed

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,29 +4,26 @@ import {Bars3BottomRightIcon} from "@heroicons/react/24/solid"
 
 function Header() {
   return (
-    <div className="gird grid-cols-2 md:grid-cols-5 justify-between items-center p-5">
+    <header className="grid grid-cols-2 md:grid-cols-5 justify-between items-center p-5">
         <div className="flex item-center space-x-2">
             <img 
             className="rounded-full h-20 w-20"
             src="https://i.pinimg.com/originals/8d/af/b4/8dafb4099c6ff7d770a279be6cf84eb1.jpg" 
             alt= "" 
             />
-        </div>
         <div>
             <h1 className="text-lg text-white font-bold ">Crypto Lottery Draw</h1>
             <p className="text-xs text-emerald-500 truncate">
                 User: ...
             </p>
         </div>
-        <div>
+        </div>
             <div className=" hidden md:flex md:col-span-3 items-center justify-center rounded-md">
                 <div className='bg-[#0A1F1C] p-4 space-x-2'>
                     <NavButton isActive title="Buy Tickets" />
                     <NavButton title="Logout" />
-
                 </div>
             </div>
-        </div>
 
         <div className="flex flex-col ml-auto text-right">
             <Bars3BottomRightIcon className='h-8 w-8 mx-auto text-white cursor-pointer'/>
@@ -34,13 +31,8 @@ function Header() {
             <NavButton title="Logout" />
             </span>
         
-        
         </div>
-
-       
-
-
-    </div>
+    </header>
   )
 }
 
