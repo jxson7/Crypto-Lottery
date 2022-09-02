c6d77ed23981d020b870b21a045278ccdb306ff5
JASON DOOKARUN <jason.dookarun@live.co.uk>
2022-09-02 16:41:08 +0100

    addition of custom styles for ticket stats

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,6 +13,7 @@ import Image from 'next/image'
 import Header from '../components/Header'
 import Loading from '../components/Loading'
 import Login from '../components/Login'
+import { useState } from 'react'
 
 
 /**
@@ -23,6 +24,7 @@ import Login from '../components/Login'
 const Home: NextPage = () => {
 
   const address = useAddress();
+  const [quantity, setQuantity] = useState<number>(1);
   const {contract, isLoading} = useContract(process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS);
   console.log(address);
 
@@ -45,6 +47,75 @@ const Home: NextPage = () => {
 
       <Header />
 
+    {/* next draw box */}
+
+      <div className="space-y-5 md:space-y-0 m-5 md:flex md:flex-row items-start justify-center md:space-x-5">
+        <div className = "stats-container">
+        <h1 className='text-5xl text-white font-semibold text-center'>The Next Draw</h1>
+        <div className=' flex justify-between p-2 space-x-2'>
+          <div className="stats">
+            <h2 className='text-sm '> Total Pool </h2>
+            <p className = "text-xl"> 0.1 MATIC</p>
+          </div>
+            <div className="stats">
+            <h2 className='text-sm '> Tickets Remaining</h2>
+            <p className='text-xl'> 100 Tickets Remaining</p>
+          </div>
+        </div>
+        {/* countdown timer */}
+        </div>
+        
+
+          <div className="stats-container space-y-2">
+          <div className="stats-container">
+          <div className='flex justify-between items-center text-white pb-2'>
+            <h2>
+              Price Per Ticket
+            </h2>
+            <p>0.01 MATIC</p>
+          </div>
+
+
+          <div className='flex text-white items-center space-x-2 bg-[#091B18] border-[#004337] border p-4'>
+            <p> TICKETS</p>
+            <input className="flex w-full bg-transparent text-right outline-none" 
+            type="number" 
+            min={1}
+            max={10}
+            value = {quantity}
+            onChange= {e => setQuantity(Number(e.target.value))}
+            />
+          </div>
+
+
+          <div className='space-y-2 mt-5'>
+            <div className='flex items-center justify-between text-emerald-300 text-sm italic font-extrabold'>
+              <p>Total Cost of Tickets</p>
+              <p>0.999</p>
+            </div>
+            <div className='flex items-center justify-between text-emerald-300 text-xs italic'>
+              <p>Service Fees</p>
+              <p>0.001 MATIC</p>
+            </div>
+            <div className='flex items-center justify-between text-emerald-300 text-xs italic'>
+              <p>+ Network Fees</p>
+              <p>TBC </p>
+            </div>
+          </div>
+          <button>
+            Buy Tickets
+          </button>
+          </div>
+          </div>
+      </div>
+
+
+    {/* price per ticket box */}
+    <div>
+      <div>
+        
+      </div>
+    </div>
 
     </div>
   )
