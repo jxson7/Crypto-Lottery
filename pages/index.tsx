da67466156b59bce5805b3785f88a74132e6f1df
JASON DOOKARUN <jason.dookarun@live.co.uk>
2022-09-02 14:46:13 +0100

    code refactor to introduce Loading.tsx method

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,12 +6,12 @@ import {
   useContractData,
   useContractCall,
   useAccount,
-
 } from '@thirdweb-dev/react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../components/Header'
+import Loading from '../components/Loading'
 import Login from '../components/Login'
 
 
@@ -23,9 +23,17 @@ import Login from '../components/Login'
 const Home: NextPage = () => {
 
   const address = useAddress();
+  const {contract, isLoading} = useContract(process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS);
   console.log(address);
 
+
   if(!address) return (<Login/>)
+
+
+  if(isLoading) return(
+    <Loading/>
+    
+  );
 
 
   return (
