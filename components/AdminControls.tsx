3b0c78e5fb86f31892f79d964a074cb6a2382488
JASON DOOKARUN <jason.dookarun@live.co.uk>
2022-09-07 23:11:42 +0100

    update to main admin screenshot

--- components/AdminControls.tsx
+++ components/AdminControls.tsx
@@ -17,23 +17,23 @@ import toast from 'react-hot-toast';
 
 function AdminControls() {
 
-    const {contract, isLoading} = useContract(process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS); 
-    const {data: totalCommission} = useContractData(contract, "operatorTotalCommission");
+    const { contract, isLoading } = useContract(process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS);
+    const { data: totalCommission } = useContractData(contract, "operatorTotalCommission");
 
-    const {mutateAsync: DrawWinnerTicket} = useContractCall(contract, "DrawWinnerTicket");
-    const {mutateAsync: RefundAll} = useContractCall(contract, "RefundAll");
-    const {mutateAsync: restartDraw} = useContractCall(contract, "restartDraw");
-    const {mutateAsync: WithdrawCommission} = useContractCall(contract, "WithdrawCommission");
+    const { mutateAsync: DrawWinnerTicket } = useContractCall(contract, "DrawWinnerTicket");
+    const { mutateAsync: RefundAll } = useContractCall(contract, "RefundAll");
+    const { mutateAsync: restartDraw } = useContractCall(contract, "restartDraw");
+    const { mutateAsync: WithdrawCommission } = useContractCall(contract, "WithdrawCommission");
 
 
     const drawWinner = async () => {
         const notification = toast.loading("Drawing winner...");
         try {
-            const data =  await DrawWinnerTicket([{}]);
-            toast.success("Winner drawn successfully!", {id: notification});
+            const data = await DrawWinnerTicket([{}]);
+            toast.success("Winner drawn successfully!", { id: notification });
         }
-        catch (err){
-            toast.error("Error drawing winner", {id: notification});
+        catch (err) {
+            toast.error("Error drawing winner", { id: notification });
             console.error("Contract failure", err);
         }
     }
@@ -41,11 +41,11 @@ function AdminControls() {
     const onRestartDraw = async () => {
         const notification = toast.loading("Restarting draw...");
         try {
-            const data =  await restartDraw([{}]);
-            toast.success("Draw restarted successfully!", {id: notification});
+            const data = await restartDraw([{}]);
+            toast.success("Draw restarted successfully!", { id: notification });
         }
-        catch (err){
-            toast.error("Error restarting draw", {id: notification});
+        catch (err) {
+            toast.error("Error restarting draw", { id: notification });
             console.error("Contract failure", err);
         }
     }
@@ -55,22 +55,22 @@ function AdminControls() {
         const notification = toast.loading("Withdrawing commission...");
         try {
             const data = await WithdrawCommission([{}]);
-            toast.success("Commission withdrawn successfully!", {id: notification});
+            toast.success("Commission withdrawn successfully!", { id: notification });
         }
-        catch (err){
-            toast.error("Error withdrawing commission", {id: notification});
+        catch (err) {
+            toast.error("Error withdrawing commission", { id: notification });
             console.error("Contract failure", err);
         }
-    } 
-    
+    }
+
     const onRefundAll = async () => {
         const notification = toast.loading("Refunding all...");
         try {
             const data = await RefundAll([{}]);
-            toast.success("Refunded all successfully!", {id: notification});
+            toast.success("Refunded all successfully!", { id: notification });
         }
-        catch (err){
-            toast.error("Error refunding all", {id: notification});
+        catch (err) {
+            toast.error("Error refunding all", { id: notification });
             console.error("Contract failure", err);
         }
     }
@@ -78,32 +78,32 @@ function AdminControls() {
 
 
 
-    
 
-  return (
-    <div className='text-white text-center px-5 py-3 rounded-md border-emerald-300/20 border'>
-        <h2 className='font-bold'>Admin Controls</h2>
-        <p className='mb-5'>Total Commission to be withdrawn:{" "}   
-        {totalCommission && ethers.utils.formatEther(totalCommission.toString())}{" "}{currency} 
-        </p>
 
-        <div className='flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2'>
-            <button onClick={drawWinner}className='admin-buttons'> 
-                <StarIcon className='h-6 mx-auto mb-2'/>
-                Draw Winner</button>
-            <button onClick={onWithdrawCommission}className='admin-buttons'> 
-                <CurrencyDollarIcon className='h-6 mx-auto mb-2'/>
-                Withdraw Commission</button>
-            <button onClick={onRestartDraw}className='admin-buttons'> 
-                <ArrowPathIcon className='h-6 mx-auto mb-2'/>
-                Restart Draw</button>
-            <button onClick={onRefundAll}className='admin-buttons'> 
-                <ArrowUturnDownIcon className='h-6 mx-auto mb-2'/>
-                Refund All</button>
+    return (
+        <div className='text-white text-center px-5 py-3 rounded-md border-emerald-300/20 border'>
+            <h2 className='font-bold'>Admin Controls</h2>
+            <p className='mb-5'>Total Commission to be withdrawn:{" "}
+                {totalCommission && ethers.utils.formatEther(totalCommission.toString())}{" "}{currency}
+            </p>
 
+            <div className='flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2'>
+                <button onClick={drawWinner} className='admin-buttons'>
+                    <StarIcon className='h-6 mx-auto mb-2' />
+                    Draw Winner</button>
+                <button onClick={onWithdrawCommission} className='admin-buttons'>
+                    <CurrencyDollarIcon className='h-6 mx-auto mb-2' />
+                    Withdraw Commission</button>
+                <button onClick={onRestartDraw} className='admin-buttons'>
+                    <ArrowPathIcon className='h-6 mx-auto mb-2' />
+                    Restart Draw</button>
+                <button onClick={onRefundAll} className='admin-buttons'>
+                    <ArrowUturnDownIcon className='h-6 mx-auto mb-2' />
+                    Refund All</button>
+
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default AdminControls
--- components/CountdownTimer.tsx
+++ components/CountdownTimer.tsx
@@ -13,66 +13,66 @@ type Props = {
 }
 
 function CountdownTimer() {
-   const {contract} = useContract(process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS);
-   const { data: expiration } = useContractData(contract, "expiration");
+  const { contract } = useContract(process.env.NEXT_PUBLIC_LOTTERY_CONTRACT_ADDRESS);
+  const { data: expiration } = useContractData(contract, "expiration");
 
 
-   const renderer = ({ hours, minutes, seconds, completed } : Props) => {
-    if (completed){
-      return(
+  const renderer = ({ hours, minutes, seconds, completed }: Props) => {
+    if (completed) {
+      return (
         <div>
-        <h2 className='text-white text-center text-bold text-xl animate-bounce'>
-          Ticket Sales have now closed for this draw
-        </h2>
-        <div className='flex space-x-6'>
-          <div className='flex-1'>
-            <div className='countdown animate-pulse'>{hours}</div>
-            <div className='countdown-label'>hours</div>
-          </div>
-          <div className='flex-1'>
-            <div className='countdown animate-pulse'>{minutes}</div>
-            <div className='countdown-label'>minutes</div>
-          </div>
-          <div className='flex-1'>
-            <div className='countdown animate-pulse'>{seconds}</div>
-            <div className='countdown-label'>seconds</div>
+          <h2 className='text-white text-center text-bold text-xl animate-bounce'>
+            Ticket Sales have now closed for this draw
+          </h2>
+          <div className='flex space-x-6'>
+            <div className='flex-1'>
+              <div className='countdown animate-pulse'>{hours}</div>
+              <div className='countdown-label'>hours</div>
+            </div>
+            <div className='flex-1'>
+              <div className='countdown animate-pulse'>{minutes}</div>
+              <div className='countdown-label'>minutes</div>
+            </div>
+            <div className='flex-1'>
+              <div className='countdown animate-pulse'>{seconds}</div>
+              <div className='countdown-label'>seconds</div>
+            </div>
           </div>
         </div>
-      </div>
 
       );
-      
-    } else{
-      return(
+
+    } else {
+      return (
         <div>
-        <h3 className='text-white text-sm mb-2 italic'>
-          Time Remaining
-        </h3>
-        <div className='flex space-x-6'>
-          <div className='flex-1'>
-            <div className='countdown'>{hours}</div>
-            <div className='countdown-label'>hours</div>
-          </div>
-          <div className='flex-1'>
-            <div className='countdown'>{minutes}</div>
-            <div className='countdown-label'>minutes</div>
-          </div>
-          <div className='flex-1'>
-            <div className='countdown'>{seconds}</div>
-            <div className='countdown-label'>seconds</div>
+          <h3 className='text-white text-sm mb-2 italic'>
+            Time Remaining
+          </h3>
+          <div className='flex space-x-6'>
+            <div className='flex-1'>
+              <div className='countdown'>{hours}</div>
+              <div className='countdown-label'>hours</div>
+            </div>
+            <div className='flex-1'>
+              <div className='countdown'>{minutes}</div>
+              <div className='countdown-label'>minutes</div>
+            </div>
+            <div className='flex-1'>
+              <div className='countdown'>{seconds}</div>
+              <div className='countdown-label'>seconds</div>
+            </div>
           </div>
         </div>
-      </div>
       )
-     
+
     }
 
-   };
+  };
 
 
   return (
     <div>
-      <Countdown date={new Date(expiration * 1000)} renderer={renderer}/>
+      <Countdown date={new Date(expiration * 1000)} renderer={renderer} />
     </div>
   )
 }
