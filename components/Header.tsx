c25186e16cb28240da0f17626555df82b8fe5233
JASON DOOKARUN <jason.dookarun@live.co.uk>
2022-09-07 23:11:51 +0100

    update to mainAdmin

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import NavButton from './NavButton';
-import {Bars3BottomRightIcon} from "@heroicons/react/24/solid"
+import { Bars3BottomRightIcon } from "@heroicons/react/24/solid"
 import { useAddress, useDisconnect } from '@thirdweb-dev/react';
 
 function Header() {
@@ -9,21 +9,21 @@ function Header() {
     const disconnect = useDisconnect();
 
 
-  return (
-    <header className="grid grid-cols-2 md:grid-cols-5 justify-between items-center p-5">
-        <div className="flex item-center space-x-2">
-            <img 
-            className="rounded-full h-20 w-20"
-            src="https://i.ibb.co/yRrSkyC/IMG-0093.jpg" 
-            alt= "" 
-            />
-        <div>
-            <h1 className="text-lg text-white font-bold ">Crypto Lottery Draw</h1>
-            <p className="text-xs text-emerald-500 truncate">
-                User: {address?.substring(0,5)}...{address?.substring(address.length - 5)}
-            </p>
-        </div>
-        </div>
+    return (
+        <header className="grid grid-cols-2 md:grid-cols-5 justify-between items-center p-5">
+            <div className="flex item-center space-x-2">
+                <img
+                    className="rounded-full h-20 w-20"
+                    src="https://i.ibb.co/yRrSkyC/IMG-0093.jpg"
+                    alt=""
+                />
+                <div>
+                    <h1 className="text-lg text-white font-bold ">Crypto Lottery Draw</h1>
+                    <p className="text-xs text-emerald-500 truncate">
+                        User: {address?.substring(0, 5)}...{address?.substring(address.length - 5)}
+                    </p>
+                </div>
+            </div>
             <div className=" hidden md:flex md:col-span-3 items-center justify-center rounded-md">
                 <div className='bg-[#0A1F1C] p-4 space-x-2'>
                     <NavButton isActive title="Buy Tickets" />
@@ -31,15 +31,15 @@ function Header() {
                 </div>
             </div>
 
-        <div className="flex flex-col ml-auto text-right">
-            <Bars3BottomRightIcon className='h-8 w-8 mx-auto text-white cursor-pointer'/>
-            <span className='md:hidden'>
-            <NavButton onClick={disconnect} title="Logout" />
-            </span>
-        
-        </div>
-    </header>
-  )
+            <div className="flex flex-col ml-auto text-right">
+                <Bars3BottomRightIcon className='h-8 w-8 mx-auto text-white cursor-pointer' />
+                <span className='md:hidden'>
+                    <NavButton onClick={disconnect} title="Logout" />
+                </span>
+
+            </div>
+        </header>
+    )
 }
 
 export default Header;
